9ca66b7e3832c8b5a7a5b33f9b0d574cdb675ab8
goga iremashvili <goga.iremashvili2000@gmail.com>
2020-08-31 22:24:21 +0400

    Messiging

---
+++ src/ServerTest.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class ServerTest {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Server sally=new Server();
+		sally.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sally.startRunning();
+		
+
+	}
+
+}
